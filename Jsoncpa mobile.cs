9b506bbdc1c6deec6a8ab7f53c19801730a45e96
omartkd7 <70722354+omartkd7@users.noreply.github.com>
2022-03-02 05:47:52 -0800

    Delete Jsoncpa mobile.cs

--- Jsoncpa mobile.cs
+++
@@ -1,8 +0,0 @@
-// Json data format
-/*
-      {
-        "Name"     : "testcpa",
-        "ImageURL" : "https://drive.google.com/uc?export=download&id=1OfOXFxHTjqo3B6dzr-jn_5yqpNOi5t7C"
-        "URL"      : "https://pastebin.com"
-      }
-*/
